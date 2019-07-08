96026e7c44654fae68775a15cc48415151af9e56
zhangkun <zkudpiao@126.com>
2019-07-08 15:20:21 +0800

    20190708

--- student/views.py
+++ student/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from student.models import Student
-
+from student.forms import StudentForm
 
 def index(request):
-    students = Student.objects.all()
-    return render(request, 'index.html', context={'students': students})
+    students = Student.get_all()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = StudentForm()
+    context = {
+        'students': students,
+        'form': form,
+    }
+    return render(request, 'index.html', context=context)
+
